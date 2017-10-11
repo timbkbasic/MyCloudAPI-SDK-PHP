e679c2461a3b448d4fe62a87b0cac38de883f6ee
Tim Endres <tim@bkbasic.com>
2017-10-11 16:25:51 -0400

    Fixed composer related issues.

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,20 +1,34 @@
 <?php
 
-$vendorPath = dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'vendor';
-$composerAutoload = $vendorPath . DIRECTORY_SEPARATOR . 'autoload.php';
+function root_dir( $dir, $levels ) {
+	if ( $levels == 0 ) {
+		return $dir;
+	} else {
+		return root_dir( dirname($dir), --$levels );
+	}
+}
 
 $reason = '';
 $loadable = TRUE;
-if ( ! file_exists($vendorPath) ) {
-	$loadable = FALSE;
-	$reason = "The 'vendor' folder is missing. (expected at '" . $vendorPath . "')";
-}
+
+// First, we will assume that this is a standard composer distribution and
+// look for the autoload.php in that context...
+$topDir = root_dir( __FILE__, 2 );
+print "1) Path[top] '" . $topDir . "'" . PHP_EOL;
+$composerAutoload = $topDir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 if ( ! file_exists($composerAutoload) ) {
-	$loadable = FALSE;
-	$reason = "The 'autoload.php' file is missing. (expected at '" . $composerAutoload . "')";
+	print "   '" . $composerAutoload . "' NOT FOUND". PHP_EOL;
+	$topDir = root_dir( __FILE__, 1 );
+	print "2) Path[top] '" . $topDir . "'" . PHP_EOL;
+	$composerAutoload = $topDir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+	if ( ! file_exists($composerAutoload) ) {
+		$loadable = FALSE;
+		$reason = "The 'autoload.php' file was not found. (expected at '" . $composerAutoload . "')";
+	}
 }
 
 if ( $loadable ) {
+	"Loading: '" . $composerAutoload . "'" . PHP_EOL;
 	require $composerAutoload;
 } else {
 	print "Could not bootstrap the example program:" . PHP_EOL;
